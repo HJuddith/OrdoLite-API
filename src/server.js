f99e0e9fe34fd41c58858af4db8b896d3f7179c4
HJuddith <jtserisoa@gmail.com>
2025-08-27 15:42:13 +0200

    update app

--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
-import dotenv from 'dotenv';
-import './config/env.js';
-import { app, logger } from './app.js';
-import http from 'http';
-import { initSocket } from './socket/index.js';
+import dotenv from "dotenv";
+import "./config/env.js";
+import { app, logger } from "./app.js";
+import http from "http";
+import { initSocket } from "./socket/index.js";
 
-dotenv.config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
+dotenv.config({ path: `.env.${process.env.NODE_ENV || "development"}` });
 
 const server = http.createServer(app);
 
 // Initialisation Socket.IO
 initSocket(server);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 server.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
   console.log(`Serveur démarré sur le port ${PORT}`);
